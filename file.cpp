5ff589a173d1c3b7c904c2b7a54c839ac8889f57
NileshVirkar148 <75842704+NileshVirkar148@users.noreply.github.com>
2025-07-21 22:49:42 +0530

    Update file.cpp

--- file.cpp
+++ file.cpp
@@ -46,6 +46,8 @@ void example_function() {
     }
 }
 
+void test(int z) { if (z == 0) int x = 1 / z; // warn }
+
 int main() {
     create_memory_leak();
     std::cout << "The memory allocated in the function is now leaked." << std::endl;
