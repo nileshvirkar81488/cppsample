c0f4e552a052b3e2d805ba809cdef7aac48b35c4
NileshVirkar148 <75842704+NileshVirkar148@users.noreply.github.com>
2025-07-22 16:56:12 +0530

    Add files via upload

---
+++ cppfiles/StaticLib1.cpp
@@ -0,0 +1,111 @@
+// StaticLib1.cpp : Defines the functions for the static library.
+//
+
+#include "pch.h"
+#include "framework.h"
+#include <stdio.h>
+#include <string.h>
+#include <array>
+///#include "comutil.h"
+#include <vector>
+//#include "atlcomcli.h"
+using namespace std;
+
+
+
+class SimpleAdder
+{
+public:
+	int Add(int* a, int* b)
+	{
+		return( *a + *b);
+	}
+
+	int Add()
+	{
+		return(*a + *b);
+	}
+
+	SimpleAdder()
+	{
+
+	}
+
+	SimpleAdder(int x,int y)
+	{
+		a = new int(x);
+		b = new int(y);	
+	}
+
+	~SimpleAdder()
+	{
+		/*if (a != nullptr)
+		{
+			delete a;
+			a = nullptr;
+		}
+		if (b != nullptr)
+		{
+			delete b;
+			b = nullptr;
+		}*/
+	}
+
+private:
+	int* a = nullptr;
+	int* b = nullptr;
+};
+
+static int Add(int* p1, int* p2)
+{
+	int sum = 0;
+
+	sum = *p1 + *p2;
+
+	return sum;
+}
+
+// TODO: This is an example of a library function
+void fnStaticLib1()
+{
+
+	char* temp = new char[100];	
+	strcpy_s(temp, 100, "Hello from StaticLib1!");
+	// resource leak
+
+	char x[4];
+	const char* y = "abcddfdf";
+	strcpy(x, y); // buffer overflow
+
+
+	char a[4];	
+	char temp1[26] = "abcdefghijklmnopqrstuvwxy";
+	strcpy(a, temp1); // buffer overflow
+
+	//BSTR bstr = SysAllocString(L"Hello from StaticLib1!"); // resource leak	
+	
+	
+		
+
+	int* p1 = new int(10);
+	int* p2 = nullptr;
+
+	SimpleAdder*	adder = new SimpleAdder();
+	int result = adder->Add(p1, p2); // dereferncing a null pointer variable
+	int sum = Add(p1, p2);
+
+
+	SimpleAdder* adder1 = new SimpleAdder(9,9);
+	adder1->Add();
+
+	/*std::vector<CComBSTR> vec;
+	char token[256] = "fksdfdshfhdfdbf";
+	BSTR bstrToken = _com_util::ConvertStringToBSTR(token);
+	vec.push_back(bstrToken);*/
+
+	return;
+
+}
+
+
+
